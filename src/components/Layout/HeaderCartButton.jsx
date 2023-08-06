85db69ad31641b316b083790245acc59672ee45b
Henrique <henriquepinheiroxavier@gmail.com>
2023-08-05 23:52:32 -0300

    feat: add cart provider and add/remove items

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,15 +1,23 @@
 import PropTypes from 'prop-types';
+import { useContext } from 'react';
 import CartIcon from "../Cart/CartIcon";
 import classes from './HeaderCartButton.module.css';
+import CartContext from '../../store/Cart-context';
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const number0fCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{number0fCartItems}</span>
     </button>
   );
 };
